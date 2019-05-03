e1943142b9d77b7406ae886af9f4392858b00934
stevebrown6468 <ananoz.stevebrown@gmail.com>
2019-05-03 07:41:14 +0300

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+//check Off Specific Todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");	
+});
+
+//click on X to delete todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//makes empty new string
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	};
+});
